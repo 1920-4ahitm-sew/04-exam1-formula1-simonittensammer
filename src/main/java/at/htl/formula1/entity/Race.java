d0ae3432ada60dfcd84b3bbf63350d5c227006cd
Simon <simon.ittensammer@gmx.at>
2019-11-22 12:34:01 +0100

    VERB: remove wuerstelcode

--- src/main/java/at/htl/formula1/entity/Race.java
+++ src/main/java/at/htl/formula1/entity/Race.java
@@ -11,8 +11,14 @@ import java.time.LocalDate;
 @Entity
 @Table(name = "F1_RACE")
 @NamedQueries({
-        @NamedQuery(name = "Race.getById", query = "select r from Race r where r.id = :ID"),
-        @NamedQuery(name = "Race.getByCountry", query = "select r from Race r where r.country = :COUNTRY")
+        @NamedQuery(
+                name = "Race.getById",
+                query = "select r from Race r where r.id = :ID"
+        ),
+        @NamedQuery(
+                name = "Race.getByCountry",
+                query = "select r from Race r where r.country = :COUNTRY"
+        )
 })
 public class Race {
 
